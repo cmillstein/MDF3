57ee5fa445b53f15aa7484888733f1a74d2085be
cmillstein <cmillstein@fullsail.edu>
2016-01-04 13:56:33 -0500

    Add Project 1

---
+++ MyApplication/app/src/androidTest/java/com/example/caseymillstein/c_millstein_mdf3fundamentals/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.caseymillstein.c_millstein_mdf3fundamentals;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
